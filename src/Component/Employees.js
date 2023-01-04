a34b8386be9e910daff140f21a9d2d00180fc3a7
mithanisalman001 <mithanisalman001@gmail.com>
2023-01-04 10:43:48 +0500

    adding files

---
+++ src/Component/Employees.js
@@ -0,0 +1,17 @@
+const Employees = [
+    {
+        id:"1",
+        Name:"Person1",
+        Age:"20"
+    },{
+        id:"2",
+        Name:"Person2",
+        Age:"25"
+    },{
+        id:"3",
+        Name:"Person3",
+        Age:"27"
+    }
+]
+
+export default Employees;
